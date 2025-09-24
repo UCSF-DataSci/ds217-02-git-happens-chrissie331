899d415f5193739ac278ca0d0d40c3394201b12d
chrissie331 <220555900+chrissie331@users.noreply.github.com>
2025-09-24 15:59:39 -0700

    Created student.csv

--- setup_project.sh
+++ setup_project.sh
@@ -7,6 +7,23 @@ touch .gitignore
 touch requirements.txt 
 echo "files created"
 
+cat > data/student.csv << 'EOF'
+Liz 23, 50, Math 
+Emma 23, 100, Art 
+Rebekah 22, 90, History 
+Chrissie 23, 95, Biology 
+Debbie 21, 88, Physics 
+Melody 25, 79, English 
+Sara 24, 80, Organic Chemistry 
+Will 26, 89, Social Studies 
+EOF
+
+
+
+
+chmod +x setup_project.sh
+
+
 
 
 
